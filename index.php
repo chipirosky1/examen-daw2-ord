fef01638cc9083bbe5492f791ad2f66b0acff39a
PerezBartolome <134142929+chipirosky1@users.noreply.github.com>
2025-03-09 12:51:52 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    $nombre = "";?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $nombre; ?> - DAW2 - ORDINARIA</title>
+</head>
+<body>
+    <h1>PRACTICA DAW – DAW2 - ORDINARIA</h1>
+
+    <h2>PARTE 2</h2>
+    <ol>
+        <li>Deberás tener un fichero con los pantallazos del examen anterior, crea una nueva rama (tuNombre_branchORD) del repositorio clonado</li>
+        <li>MODIFICA ESTE archivo para que el titulo sea tu nombre y apellidos.</li>
+        <li>Crea un nuevo pantallazo al WORD que estas creando del examen con el paso anterior.</li>
+        <li>Añade el fichero WORD a la rama que has creado, el fichero deberá llamarse Nombre_Apellido_DAW2_ORD</li>
+        <li>Sube tu rama al repositorio</li>
+        <li>Por último sube también el fichero al aula virtual.</li>
+    </ol>
+</body>
+</html>
